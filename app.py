c17497c46503f7813147f9c7eb851d4ddd8f44e0
Huarada <121201067+Huarada@users.noreply.github.com>
2024-11-04 10:48:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -240,3 +240,7 @@ def sell():
 
         flash("Sold")
         return redirect("/")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render define automaticamente a variável PORT
+    app.run(host="0.0.0.0", port=port)
