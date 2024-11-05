83c1dd074a6d2e210705c2b15f959b27f1dafd27
Huarada <121201067+Huarada@users.noreply.github.com>
2024-11-05 17:20:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def sell():
 
         transaction_value = shares * stock["price"]
         user_id = session["user_id"]
-        user_cash_db = db.execute("SELECT cash FROM users WHERE id =: id", id=user_id)
+        user_cash_db = db.execute("SELECT cash FROM users WHERE id = :id", id=user_id)
         user_cash = user_cash_db[0]["cash"]
 
         updt_cash = user_cash + transaction_value
